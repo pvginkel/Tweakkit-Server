af804a3f7749f0ba081b70a0083352c4a1c79b52
md_5 <md_5@live.com.au>
2013-07-02 09:07:54 +1000

    Lower Chunk Compression
    Use a chunk compression level of 4 - this provides an optimal balance between speed and compression.

--- src/main/java/net/minecraft/server/PacketPlayOutMapChunkBulk.java
+++ src/main/java/net/minecraft/server/PacketPlayOutMapChunkBulk.java
@@ -22,7 +22,7 @@ public class PacketPlayOutMapChunkBulk extends Packet {
         @Override
         protected Deflater initialValue() {
             // Don't use higher compression level, slows things down too much
-            return new Deflater(6);
+            return new Deflater(4); // Spigot 6 -> 4
         }
     };
     // CraftBukkit end
