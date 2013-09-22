b0034047f95e086d827b8df803f6d93c4ab80ae6
md_5 <md_5@live.com.au>
2013-09-22 19:10:53 +1000

    Item Despawn Rate

--- src/main/java/net/minecraft/server/EntityItem.java
+++ src/main/java/net/minecraft/server/EntityItem.java
@@ -104,7 +104,7 @@ public class EntityItem extends Entity {
             }
 
             // ++this.age; // CraftBukkit - Moved up
-            if (!this.world.isStatic && this.age >= 6000) {
+            if (!this.world.isStatic && this.age >= world.spigotConfig.itemDespawnRate) { // Spigot
                 // CraftBukkit start - fire ItemDespawnEvent
                 if (org.bukkit.craftbukkit.event.CraftEventFactory.callItemDespawnEvent(this).isCancelled()) {
                     this.age = 0;
