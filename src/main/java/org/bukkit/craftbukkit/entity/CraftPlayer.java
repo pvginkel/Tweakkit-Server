7cd09992ff1cb32f7e1d206c14c961eee307163d
ninja- <xninja@openmailbox.org>
2013-10-08 14:34:49 +0200

    Implement respawn API.

--- src/main/java/org/bukkit/craftbukkit/entity/CraftPlayer.java
+++ src/main/java/org/bukkit/craftbukkit/entity/CraftPlayer.java
@@ -1293,6 +1293,15 @@ public class CraftPlayer extends CraftHumanEntity implements Player {
             getHandle().collidesWithEntities = collides;
             getHandle().k = collides; // First boolean of Entity
         }
+
+        @Override
+        public void respawn()
+        {
+            if ( getHealth() <= 0 && isOnline() )
+            {
+                server.getServer().getPlayerList().moveToWorld( getHandle(), 0, false );
+            }
+        }
     };
 
     public Player.Spigot spigot()
